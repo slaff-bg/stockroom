e1c4b89f16eddaa03083275488f1b8643ff3b3a8
Slaff <slaff.bg@gmail.com>
2023-02-22 23:40:16 +0200

    Main structure.

---
+++ src/graphql/domain/categories.go
@@ -0,0 +1,15 @@
+package domain
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Category struct {
+	ID          uuid.UUID `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
